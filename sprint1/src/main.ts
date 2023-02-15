b0d6fd9754110b11966ba223941ce1dc50494f2c
Julia Fu <juliafu@Julias-MacBook-Pro-957.local>
2023-02-15 10:28:57 -0500

    style

--- sprint1/src/main.ts
+++ sprint1/src/main.ts
@@ -1,24 +1,15 @@
 import * as mockData from "./mockFiles/mockCSV.js"
 import { csvFile as csvFile } from "./mockFiles/mockCSV.js";
-// The window.onload callback is invoked when the window is first loaded by the browser
+
+// prepare entered command and submit button
 window.onload = () => {    
     prepareinput();
     prepareSubmitPress();  
-    // If you're adding an event for a button click, do something similar.
-    // The event name in that case is "click", not "keypress", and the type of the element 
-    // should be HTMLButtonElement. The handler function for a "click" takes no arguments.
 }
 
+// default mode is brief
 let current_mode = "Brief";
 
-// function prepareCommand(){
-//     const maybeInputs: null | HTMLCollectionOf<Element> = document.getElementsByClassName('repl-command-box')
-//     if (maybeInputs == null){
-//         return ("Couldn't find input element")
-//     }else {
-//         maybeInputs.addEventListener("keypress", handleKeypress)
-//     }
-// }
 function prepareinput(){
     const input = document.getElementsByClassName("repl-command-box")[0];
     if(input == null) {
@@ -26,42 +17,37 @@ function prepareinput(){
     } else if(!(input instanceof HTMLInputElement)) {
         console.log(`Found element ${input}, but it wasn't an input`);
     } else {
-        // Notice that we're passing *THE FUNCTION* as a value, not calling it.
-        // The browser will invoke the function when a key is pressed with the input in focus.
-        //  (This should remind you of the strategy pattern things we've done in Java.)
         input.addEventListener('keydown', (e) => {
             if (e.key == "Enter"){
+                // strategy 1: reads the entered command after pressing "Enter"
                 read();
             }
         });
     }
 }
 
+// strategy 2: deals with entered command after pressing Submit
 function prepareSubmitPress() {
-    // As far as TypeScript knows, there may be *many* elements with this class.
     const maybeInputs: HTMLCollectionOf<Element> = document.getElementsByClassName("enter-command");
-    // Assumption: there's only one thing
     const maybeInput: Element | null = maybeInputs[0];
-    // Is the thing there? Is it of the expected type? 
-    //  (Remember that the HTML author is free to assign the repl-input class to anything :-) )
     if(maybeInput == null) {
         console.log("Couldn't find input element");
     } else if(!(maybeInput instanceof HTMLButtonElement)) {
         console.log(`Found element ${maybeInput}, but it wasn't an input`);
     } else {
-        // Notice that we're passing *THE FUNCTION* as a value, not calling it.
-        // The browser will invoke the function when a key is pressed with the input in focus.
-        //  (This should remind you of the strategy pattern things we've done in Java.)
         maybeInput.addEventListener("click", handleButtonClick);
     }
 }
 
+// input csv File
 let file: csvFile | undefined;
 
+// reads after clicking the Submit button
 function handleButtonClick(event: MouseEvent) {   
     read();
 }
 
+// deals with the input command
 function read(){
     const newcommand = document.getElementsByClassName("repl-command-box")[0];
     if (newcommand == null){
@@ -77,9 +63,8 @@ function read(){
 
 function handle_sentence(cmd: string){
     let repl_output = document.getElementsByClassName("output")[0];
-    let repl_header = document.getElementsByClassName("header")[0];
-    let repl_view = document.getElementsByClassName("file-content")[0];
     var output = "";
+    // User Story 1
     if (cmd == "mode"){
         if (current_mode == "Brief"){
             current_mode = "Verbose";
@@ -89,7 +74,8 @@ function handle_sentence(cmd: string){
             current_mode = "Brief";
             output =  "Mode switched to Brief. ";
         }
-    }else if (cmd.substring(0, 9) == "load_file"){
+    }// User Story 2
+    else if (cmd.substring(0, 9) == "load_file"){
         var filename = cmd.split(" ");
         if (filename.length != 2) {
            output = "Invalid number of inputs";
@@ -103,7 +89,8 @@ function handle_sentence(cmd: string){
                 output = "CSV file loaded successfully";
             }
         }
-    }else if(cmd == "view"){
+    } // User Story 3
+    else if(cmd == "view"){
         var output = "";
         var header = "";
         var rows = "";
@@ -112,19 +99,17 @@ function handle_sentence(cmd: string){
         }
         else{
             if (file.hasHeaders){
-                //repl_header.innerHTML += file.header;
                header += '<br><th>' + file.header + '</th></br>';
-               //header += file.header;
             }
             var r = 0;
             while (r < file.contents.length){
                rows += '<br><tr><td>' + file.contents[r] + '</td></tr>' + '</br>';
-               //rows += file.contents[r] ;
                r ++;
             }
             output += header + rows;
         }
-    }else if (cmd.substring(0, 6) == "search"){
+    }// User Story 4
+    else if (cmd.substring(0, 6) == "search"){
         var input = cmd.split(" ");
         var output = "";
         if (input.length != 3) {
@@ -143,7 +128,10 @@ function handle_sentence(cmd: string){
                 }
             }
         }
+    }else{
+        output = "Invalid Input";
     }
+    // Output either Brief or Verbose
     if (current_mode == "Brief"){
         repl_output.innerHTML += '<p>' + output + '</p>';
     }
@@ -155,7 +143,5 @@ function handle_sentence(cmd: string){
         repl_output.innerHTML += '<p> ERROR: Illegal Mode </p>';
     }
 }
-// Provide this to other modules (e.g., for testing!)
-// The configuration in this project will require /something/ to be exported.
 export { handleButtonClick };
 
